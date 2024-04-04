caf9acdb809f0e4cabc8676a321e337b88fdaa79
Koushik Dutta <koushik0910@gmail.com>
2024-04-04 18:07:33 +0530

    Create empty project and added .gitignore

---
+++ TheMovieDatabase_TCA/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TheMovieDatabase_TCA
+//
+//  Created by Koushik Dutta on 04/04/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TheMovieDatabase_TCA/TheMovieDatabase_TCAApp.swift
@@ -0,0 +1,17 @@
+//
+//  TheMovieDatabase_TCAApp.swift
+//  TheMovieDatabase_TCA
+//
+//  Created by Koushik Dutta on 04/04/24.
+//
+
+import SwiftUI
+
+@main
+struct TheMovieDatabase_TCAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
